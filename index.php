8c8c2e015ca68a52bb5685b88b3e63eba5756fa3
frostkom <martin.fors@frostkom.se>
2018-01-22 14:36:48 +0100

    Add licence

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Plugin Name: MF Contract
 Plugin URI: https://github.com/frostkom/mf_contracts
 Description: 
-Version: 1.0.19
+Version: 1.0.20
+Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_contract
